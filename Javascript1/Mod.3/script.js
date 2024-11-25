1c4b7d0b461f0024b7a05358ed777c39483971c8
Wesley Douglas de Assis <wesley.assis@dmrh.com.br>
2024-11-25 18:12:49 -0300

    novo commit para js

---
+++ Javascript1/Mod.3/script.js
@@ -0,0 +1,9 @@
+// Função para imprimir a mensagem no terminal
+function imprimirMensagem() {
+    console.log("Conexão feita com sucesso!");
+}
+
+// Executando a função 3 vezes
+imprimirMensagem();
+imprimirMensagem();
+imprimirMensagem();
